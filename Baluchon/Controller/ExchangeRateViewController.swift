18614e91095da94550b2080b759137610c0d858d
satanpetitcoeur <plantevin.contact@gmail.com>
2019-12-05 14:38:00 +0100

    add view [Rate, Weather, Translate] + ViewController

---
+++ Baluchon/Controller/ExchangeRateViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ExchangeRateViewController.swift
+//  Baluchon
+//
+//  Created by Edouard PLANTEVIN on 05/12/2019.
+//  Copyright © 2019 Edouard PLANTEVIN. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeRateViewController: UIViewController {
+}
---
+++ Baluchon/Controller/TranslationViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TranslationViewController.swift
+//  Baluchon
+//
+//  Created by Edouard PLANTEVIN on 05/12/2019.
+//  Copyright © 2019 Edouard PLANTEVIN. All rights reserved.
+//
+
+import UIKit
+
+class TranslationViewController: UIViewController {
+}
